70e8ba0af0952444e61f8fb327ca846d7b07b7ea
Thapelo Tsotetsi <dev.thapelo@gmail.com>
2014-06-14 15:59:25 +0200

    Project boilerplate

---
+++ public/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Liesbeeck Gardens &middot; Room allocation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="./assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 72px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="container-narrow">
+
+      <div class="masthead">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="http://www.uctlbg.co.za/">Home</a></li>
+
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <h3 class="muted">Liesbeeck Gardens(Alpha)</h3>
+      </div>
+
+      <hr>
+
+      <div class="jumbotron">
+        <h1>LBG Online!</h1>
+        <p class="lead">Make sure you familarise yourself with the room layout and allocation process on vula before proceeding</p>
+        <a class="btn btn-large btn-success" href="http://www.uctlbg.co.za/flats1.php">Proceed</a>
+      </div>
+
+      <hr>
+
+
+      <div class="footer">
+        <p> Liesbeeck Gardens &copy; 2013- <?php echo date("Y") ?> | <a href="http://www.uct.ac.za">www.uct.ac.za</a> <a style="float: right"  href="signin.php">Admin</a></p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+<!--
+   
+
+
+  </body>
+</html>
